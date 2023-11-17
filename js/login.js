8c3415f73db3934c8ccb6f64a4e3642a61ae506f
Aleksey Shumkov <shumleshas@gmail.com>
2023-11-17 21:10:17 +0700

    Добавил сверстанную страницу авторизации
    Также реализовал смену надписи Входа на Email
    
    Добавил dropdown menu

---
+++ js/login.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+
+        var email = $('#inputEmail').val();
+        var password = $('#inputPassword').val();
+
+        $.ajax({
+            type: 'POST',
+            url: 'https://blog.kreosoft.space/api/account/login',
+            contentType: 'application/json',
+            data: JSON.stringify({
+                "email": email,
+                "password": password
+            }),
+            success: function(response) {
+                var token = response.token;
+                localStorage.setItem('authToken', token);
+
+
+                var userDropdown = `
+                    <li class="nav-item dropdown">
+                      <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        ${email}
+                      </a>
+                      <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="/profile">Профиль</a>
+                        <a class="dropdown-item" href="#" id="logoutButton">Выход</a>
+                      </div>
+                    </li>
+                  `;
+
+                $('.navbar-nav.ml-auto').html(userDropdown);
+
+
+                $('#logoutButton').on('click', function() {
+                    localStorage.removeItem('authToken');
+                    location.reload();
+                });
+            },
+            error: function(xhr, status, error) {
+                alert("Ошибка!");
+            }
+        });
+    });
+});
