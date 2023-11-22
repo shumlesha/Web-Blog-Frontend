981215026ca7730f679503040351ea002ae57a8b
Aleksey Shumkov <shumleshas@gmail.com>
2023-11-22 13:57:51 +0700

    Исправил навбар на главной
    Добавил подгрузку email на главной

---
+++ js/mainpage.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+    replaceNav();
+});
+
+function replaceNav() {
+    if (localStorage.getItem('bearerToken')) {
+        getEmail(function(email) {
+            var userDropdown = `
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        ${email}
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="../html/profile.html">Профиль</a>
+                        <a class="dropdown-item" href="../html/login.html" id="logoutButton">Выход</a>
+                    </div>
+                </li>
+            `;
+
+            $('.navbar-nav.ml-auto').html(userDropdown);
+            $('#logoutButton').on('click', function() {
+                localStorage.removeItem('bearerToken');
+                location.reload();
+            });
+        });
+    }
+}
+
+function getEmail(callback) {
+    $.ajax({
+        url: 'https://blog.kreosoft.space/api/account/profile',
+        method: 'GET',
+        contentType: 'application/json',
+        headers: {
+            'Authorization': 'Bearer '+ localStorage.getItem('bearerToken')
+        },
+        success: function(response) {
+            //console.log(response.email);
+            callback(response.email);
+        },
+        error: function(xhr, status, error) {
+            console.error("Ошибка: ", status, error);
+            callback("");
+        }
+    });
+}
