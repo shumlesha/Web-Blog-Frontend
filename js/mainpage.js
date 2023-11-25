614fd4d448138039cda85dfa9aeeae7e4cb2a046
Aleksey Shumkov <shumleshas@gmail.com>
2023-11-25 17:58:46 +0700

    Сделал пагинацию
    Добавил пушинг в адр. строку
    
    Рефакторинг кода, разбил на функции

--- js/mainpage.js
+++ js/mainpage.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     replaceNav();
     fillTags();
+    ParsePosts(parseUrlOptions());
     $('#apply-filters').on('click', function(e) {
         e.preventDefault();
         var author = $('#authorSearch').val();
@@ -36,50 +37,7 @@ $(document).ready(function() {
             size: 5
         };
 
-        $.ajax({
-            url: 'https://blog.kreosoft.space/api/post',
-            method: 'GET',
-            contentType: 'application/json',
-            data: queryParams,
-            traditional: true,
-            success: function(data){
-                $.get('../html/postCard.html', function(cardPattern) {
-                    var postsContainer = $('#postsCol');
-                    postsContainer.empty();
-
-                    $.each(data.posts, function(i, post) {
-                        var createDate = post.createTime.split('T')[0].split('-').reverse().join('.');
-                        var createTime = post.createTime.split('T')[1].split(':').slice(0, 2).join(":");
-                        var communityRepr = post.communityName ? `в сообществе "${post.communityName}"` : "";
-                        var postText = post.description.length > 560
-                            ? `<span class="preview-text">${post.description.substring(0, 560)}...<br><a href="#" class="continue-read">Читать далее</a></span>
-                      
-                        <span class="full-text" style="display: none;">${post.description}<a href="#" class="hide-text">Скрыть</a></span>`
-                            : post.description;
-                        var imageTag = post.image ? `<img src="${post.image}" class="card-img-top">` : '';
-
-                        var tagsRepr = post.tags.map(tag => `#${tag.name}`).join(' ');
-
-                        var card = cardPattern.replace(/{{author}}/g, post.author)
-                            .replace(/{{date}}/g, createDate)
-                            .replace(/{{time}}/g, createTime)
-                            .replace(/{{community}}/g, communityRepr)
-                            .replace(/{{title}}/g, post.title)
-                            .replace(/{{image}}/g, imageTag)
-                            .replace(/{{text}}/g, postText)
-                            .replace(/{{tags}}/g, tagsRepr)
-                            .replace(/{{readingTime}}/g, post.readingTime)
-                            .replace(/{{commentsCount}}/g, post.commentsCount)
-                            .replace(/{{likes}}/g, post.likes);
-
-                        postsContainer.append(card);
-                    });
-                });
-            },
-            error: function(xhr, status, error) {
-                console.error("Ошибка при получении постов: ", status, error);
-            }
-        });
+        ParsePosts(queryParams);
 
     });
 });
@@ -145,6 +103,47 @@ $(document).on('click', '.hide-text', function(e) {
     //$(this).hide();
 })
 
+$('#numofPosts').change(function() {
+    var queryParams = parseUrlOptions();
+    queryParams.size = $(this).val();
+    queryParams.page = 1;
+    ParsePosts(queryParams);
+});
+
+$('.pagination a').click(function(e) {
+    e.preventDefault();
+
+    var queryParams = parseUrlOptions();
+    var currentPage = queryParams.page || 1;
+    console.log(queryParams.tags);
+
+    if ($(this).hasClass('prev')) {
+        queryParams.page = Math.max(currentPage - 1, 1);
+    }
+    else if ($(this).hasClass('next')) {
+        queryParams.page = Math.min(currentPage + 1, parseInt($('#totalPages').val()));
+        //console.log(queryParams.page);
+    }
+    else if ($(this).hasClass('page-link')) {
+        queryParams.page = $(this).data('page');
+
+    }
+
+    ParsePosts(queryParams);
+
+    reBuildPagination(queryParams.page);
+
+    var pagesCount = parseInt($('#totalPages').val());
+
+
+    $('.pagination .page-item').removeClass('active');
+    $('.pagination .page-item').each(function() {
+        if (parseInt($(this).find('.page-link').data('page')) === queryParams.page) {
+            $(this).addClass('active');
+        }
+    });
+});
+
 function  fillTags(){
     $.ajax({
         url: 'https://blog.kreosoft.space/api/tag',
@@ -159,4 +158,90 @@ function  fillTags(){
             console.error("Ошибка: ", status, error);
         }
     });
+}
+
+function ParsePosts(queryParams){
+    history.pushState(null, '', `${location.pathname}?${$.param(queryParams)}`);
+    $.ajax({
+        url: 'https://blog.kreosoft.space/api/post',
+        method: 'GET',
+        contentType: 'application/json',
+        data: queryParams,
+        traditional: true,
+        success: function(data){
+            $.get('../html/postCard.html', function(cardPattern) {
+                var postsContainer = $('#postsCol');
+                postsContainer.empty();
+
+                $.each(data.posts, function(i, post) {
+                    var createDate = post.createTime.split('T')[0].split('-').reverse().join('.');
+                    var createTime = post.createTime.split('T')[1].split(':').slice(0, 2).join(":");
+                    var communityRepr = post.communityName ? `в сообществе "${post.communityName}"` : "";
+                    var postText = post.description.length > 560
+                        ? `<span class="preview-text">${post.description.substring(0, 560)}...<br><a href="#" class="continue-read">Читать далее</a></span>
+                      
+                        <span class="full-text" style="display: none;">${post.description}<a href="#" class="hide-text">Скрыть</a></span>`
+                        : post.description;
+                    var imageTag = post.image ? `<img src="${post.image}" class="card-img-top">` : '';
+
+                    var tagsRepr = post.tags.map(tag => `#${tag.name}`).join(' ');
+
+                    var card = cardPattern.replace(/{{author}}/g, post.author)
+                        .replace(/{{date}}/g, createDate)
+                        .replace(/{{time}}/g, createTime)
+                        .replace(/{{community}}/g, communityRepr)
+                        .replace(/{{title}}/g, post.title)
+                        .replace(/{{image}}/g, imageTag)
+                        .replace(/{{text}}/g, postText)
+                        .replace(/{{tags}}/g, tagsRepr)
+                        .replace(/{{readingTime}}/g, post.readingTime)
+                        .replace(/{{commentsCount}}/g, post.commentsCount)
+                        .replace(/{{likes}}/g, post.likes);
+
+                    postsContainer.append(card);
+                });
+            });
+
+            $('#totalPages').val(data.pagination.count.toString());
+            reBuildPagination(parseInt(data.pagination.current));
+        },
+        error: function(xhr, status, error) {
+            console.error("Ошибка при получении постов: ", status, error);
+        }
+    });
+}
+
+function parseUrlOptions() {
+    var options = new URLSearchParams(window.location.search);
+    return {
+        author: options.get('author') || '',
+        tags: options.getAll('tags[]') || [],
+        sorting: options.get('sorting') || 'CreateDesc',
+        min: options.get('min') || '',
+        max: options.get('max') || '',
+        onlyMyCommunities: options.get('onlyMyCommunities') === 'true',
+        page: parseInt(options.get('page'), 10) || 1,
+        size: parseInt(options.get('size'), 10) || 5
+    };
+}
+
+
+function reBuildPagination(current){
+    var pagesCount = parseInt($('#totalPages').val());
+
+    var rangeSize = 3;
+    var currentRangeStart = Math.floor((current - 1) / rangeSize) * rangeSize + 1;
+
+    console.log(currentRangeStart);
+    $('.pagination .page-link').not('.prev, .next').each(function(index) {
+        var pageNumber = currentRangeStart + index;
+        $(this).data('page', pageNumber).text(pageNumber).parent().show();
+
+        $(this).parent().toggleClass('active', pageNumber === current);
+        $(this).parent().toggleClass('disabled', pageNumber > pagesCount);
+    });
+
+    $('.pagination .prev').parent().toggleClass('disabled', current <= 1);
+    $('.pagination .next').parent().toggleClass('disabled', current >= pagesCount);
+
 }
