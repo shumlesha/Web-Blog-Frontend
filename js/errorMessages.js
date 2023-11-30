96f91f370a3836c821c216770365863c6cc1db39
Aleksey Shumkov <shumleshas@gmail.com>
2023-11-30 19:46:23 +0700

    Добавил показ ошибок для RegisterPage

---
+++ js/errorMessages.js
@@ -0,0 +1,30 @@
+export const errorMessages = {
+    BirthDate: {
+        id: "inputBirthDate",
+        message: "Неверная дата рождения"
+    },
+    Email: {
+        id: "inputEmail",
+        message: "Неверный email"
+    },
+    FullName: {
+        id: "inputFullName",
+        message: "Неверное ФИО"
+    },
+    Gender: {
+        id: "inputGender",
+        message: "Неверный пол"
+    },
+    Password: {
+        id: "inputPassword",
+        message: "Неверный пароль"
+    },
+    PhoneNumber: {
+        id: "inputPhone",
+        message: "Неверный телефон"
+    },
+    DuplicateUserName: {
+        id: "inputEmail",
+        message: "Email уже существует"
+    }
+};
--- js/registration.js
+++ js/registration.js
@@ -1,3 +1,4 @@
+import { errorMessages } from '../js/errorMessages.js';
 $(document).ready(function() {
     $('#inputPhone').mask('+7 (000) 000-00-00', {placeholder: "+7 (xxx) xxx-xx-xx"});
     fillCreds();
@@ -19,7 +20,12 @@ $(document).ready(function() {
             success: function(response) {
                 var token = response.token;
                 localStorage.setItem('bearerToken', token);
-
+                for (var key in errorMessages) {
+                    if (errorMessages.hasOwnProperty(key)) {
+                        $('#' + errorMessages[key].id).removeClass('is-invalid');
+                        $('.invalid-feedback').empty();
+                    }
+                }
             },
             error: function(xhr, status, error) {
                 var toJson = JSON.parse(xhr.responseText);
@@ -27,23 +33,21 @@ $(document).ready(function() {
                     toJson = toJson.errors;
                 }
 
-                var errorText = Object.keys(toJson).map(function (key) {
-                    var errors = toJson[key];
-                    //return key + ": " + errors.join(", ");
-                    if (errors == null){
-                        return "";
-                    }
-                    else {
-                        if (Array.isArray(errors)) {
-                            return `key: ${errors.join(", ")}`;
-                        }
-                        else{
-                            return `key: ${errors}`;
-                        }
 
+                //alert(errorText);
+                for (var key in errorMessages) {
+                    if (errorMessages.hasOwnProperty(key)) {
+                        $('#' + errorMessages[key].id).removeClass('is-invalid');
+                        $('.invalid-feedback').empty();
                     }
-                }).join("\n").trim();
-                alert(errorText);
+                }
+                for (var key in toJson) {
+                    if (toJson.hasOwnProperty(key) && errorMessages.hasOwnProperty(key)) {
+                        var field = errorMessages[key];
+                        $('#' + field.id).addClass('is-invalid');
+                        $('#' + field.id).next('.invalid-feedback').text(toJson[key]);
+                    }
+                }
             }
         });
     });
